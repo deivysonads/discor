1dcf2a69a9638c806b086c87dd3b5dd7f9f90366
deivysonads <138238226+deivysonads@users.noreply.github.com>
2025-03-31 02:26:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const token = process.env.DISCORD_BOT_TOKEN;
-
-console.log("TOKEN =>", token);
+const roleId = process.env.DISCORD_STUDENT_ROLE_ID;
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers, // isso é essencial pra ver quem entra
+  ]
 });
 
 client.once("ready", () => {
   console.log(`✅ Bot online como ${client.user.tag}`);
 });
-console.log("TOKEN LIDO:", process.env.DISCORD_BOT_TOKEN);
+
+client.on("guildMemberAdd", async (member) => {
+  try {
+    const role = member.guild.roles.cache.get(roleId);
+    if (!role) return console.log("❌ Cargo não encontrado!");
+
+    await member.roles.add(role);
+    console.log(`✅ Cargo de aluno adicionado para ${member.user.tag}`);
+  } catch (err) {
+    console.error("Erro ao adicionar cargo:", err);
+  }
+});
+
 client.login(token);
